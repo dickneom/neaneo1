703b39644b8f7db008013eccf7ea18ef3f40e025
DickNeoM <dickneom@yahoo.com>
2017-04-25 20:56:52 -0500

    Agregando rutas 2 archivo global.js

---
+++ config/global.js
@@ -0,0 +1,7 @@
+/* cSpell:disable */
+/* jslint node: true */
+
+module.exports.USERS_FOR_PAGE = 9;
+module.exports.USERS_PASS_SIZE_MIN = 6;
+module.exports.IMAG_DRESS_ANON = 'http://res.cloudinary.com/cloud-dc/image/upload/v1487441736/brwltuenzajetyxciozo.png';
+module.exports.IMAG_USER_ANON = 'http://res.cloudinary.com/cloud-dc/image/upload/v1487441736/brwltuenzajetyxciozo.png';
